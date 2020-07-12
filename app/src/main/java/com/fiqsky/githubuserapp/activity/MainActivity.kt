4b9925f2caeac27a6a90f23e7ef79dc0e604ba4d
Fiqih Firdaus Maulana <45449889+FiqSky@users.noreply.github.com>
2020-07-12 21:52:04 +0700

    Change RecyclerView,add Localization, Etc

--- app/src/main/java/com/fiqsky/githubuserapp/activity/MainActivity.kt
+++ app/src/main/java/com/fiqsky/githubuserapp/activity/MainActivity.kt
@@ -3,19 +3,19 @@ package com.fiqsky.githubuserapp.activity
 import android.content.Intent
 import android.content.res.TypedArray
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.ListView
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fiqsky.githubuserapp.R
 import com.fiqsky.githubuserapp.User
 import com.fiqsky.githubuserapp.adapter.UserAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var adapter: UserAdapter
     private lateinit var dataName: Array<String>
     private lateinit var dataUsername: Array<String>
     private lateinit var dataLocation: Array<String>
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var dataFollowers: Array<String>
     private lateinit var dataFollowing: Array<String>
     private lateinit var dataCompany: Array<String>
-    private lateinit var fab: FloatingActionButton
     private lateinit var dataAvatar: TypedArray
     private var users = arrayListOf<User>()
 
@@ -31,25 +30,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView: ListView = findViewById(R.id.lv_main)
+        users.addAll(getListUser())
+        showRecyclerList()
 
-        adapter = UserAdapter(this)
-
-        listView.adapter = adapter
-
-        initFab()
-        prepare()
-        addItem()
-
-        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            Toast.makeText(this@MainActivity, users[position].name, Toast.LENGTH_SHORT).show()
-            val infoActivity = Intent(this@MainActivity, InfoActivity::class.java)
-            infoActivity.putExtra(InfoActivity.EXTRA_MOVIE, users[position])
-            startActivity(infoActivity)
-        }
     }
 
-    private fun prepare() {
+    private fun getListUser(): Collection<User> {
         dataName = resources.getStringArray(R.array.name)
         dataUsername = resources.getStringArray(R.array.username)
         dataLocation = resources.getStringArray(R.array.location)
@@ -58,9 +44,8 @@ class MainActivity : AppCompatActivity() {
         dataFollowers = resources.getStringArray(R.array.followers)
         dataFollowing = resources.getStringArray(R.array.following)
         dataAvatar = resources.obtainTypedArray(R.array.avatar)
-    }
 
-    private fun addItem() {
+        val listUser = ArrayList<User>()
         for (position in dataName.indices) {
             val user = User(
                 dataAvatar.getResourceId(position, -1),
@@ -74,34 +59,38 @@ class MainActivity : AppCompatActivity() {
             )
             users.add(user)
         }
-        adapter.users = users
+        return listUser
     }
 
-    private fun initFab() {
-        fab = ActivityCompat.requireViewById(this, R.id.fab)
-        val mode = AppCompatDelegate.getDefaultNightMode()
-        initFab(mode)
+    private fun showRecyclerList() {
+        rv_main.layoutManager = LinearLayoutManager(this)
+        val userAdapter = UserAdapter(users)
+        rv_main.adapter = userAdapter
+
+        userAdapter.setOnItemClickCallback(object : UserAdapter.OnItemClickCallback{
+            override fun onItemClicked(data: User) {
+                Toast.makeText(this@MainActivity, "Kamu memilih ${data.name}", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@MainActivity, InfoActivity::class.java)
+                intent.putExtra(InfoActivity.EXTRA_USER, data)
+                startActivity(intent)
+            }
+        })
     }
 
-    private fun initFab(mode: Int) {
-        when (mode) {
-            AppCompatDelegate.MODE_NIGHT_NO -> {
-                fab.setImageResource(R.drawable.ic_mode_night_no_black_24dp)
-                fab.setOnClickListener { setNightMode(AppCompatDelegate.MODE_NIGHT_YES) }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_settings -> {
+                val intent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+                startActivity(intent)
+                true
             }
-            AppCompatDelegate.MODE_NIGHT_YES -> {
-                fab.setImageResource(R.drawable.ic_mode_night_yes_black_24dp)
-                fab.setOnClickListener { setNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM) }
-            }
-            else -> {
-                fab.setImageResource(R.drawable.ic_mode_night_default_black_24dp)
-                fab.setOnClickListener { setNightMode(AppCompatDelegate.MODE_NIGHT_NO) }
-            }
+            else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    private fun setNightMode(mode: Int) {
-        AppCompatDelegate.setDefaultNightMode(mode)
-        initFab(mode)
     }
 }
